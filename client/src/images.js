b4238c48806c291ab166ab9a0d4fc54a38cf6534
FranciscoCaba <francorv2@fpuna.edu.py>
2023-03-01 14:50:37 -0300

    Imagenes agregadas con script que las exporta

---
+++ client/src/images.js
@@ -0,0 +1,62 @@
+// Exporting many images
+
+// const htmlIcon = require('./assets/icons/html5.png')
+const javaIcon = require('./assets/icons/java.png')
+const pythonIcon = require('./assets/icons/python.png')
+const djangoIcon = require('./assets/icons/django.png')
+const springIcon = require('./assets/icons/spring.png')
+const jsIcon = require('./assets/icons/js.png')
+const cSharpIcon = require('./assets/icons/c-sharp.png')
+const cssIcon = require('./assets/icons/css.png')
+const flaskIcon = require('./assets/icons/flask.png')
+const goIcon = require('./assets/icons/go.png')
+const htmlIcon = require('./assets/icons/html.png')
+const railsIcon = require('./assets/icons/rails.png')
+const reactIcon = require('./assets/icons/react.png')
+const rubyIcon = require('./assets/icons/ruby.png')
+const sqlIcon = require('./assets/icons/sql.png')
+
+export const skills = {
+    html: {
+        icon: htmlIcon, name: "html", alt: "html-icon", type: "language"
+    },
+    java: {
+        icon: javaIcon, name: "java", alt: "java-icon", type: "language"
+    },
+    python: {
+        icon: pythonIcon, name: "python", alt: "python-icon", type: "language"
+    },
+    django: {
+        icon: djangoIcon, name: "django", alt: "django-icon", type: "framework"
+    },
+    spring: {
+        icon: springIcon, name: "spring", alt: "spring-icon", type: "framework"
+    },
+    js: {
+        icon: jsIcon, name: "js", alt: "js-icon", type: "language"
+    },
+    cSharp: {
+        icon: cSharpIcon, name: "cSharp", alt: "cSharp-icon", type: "language"
+    },
+    css: {
+        icon: cssIcon, name: "css", alt: "css-icon", type: "language"
+    },
+    flask: {
+        icon: flaskIcon, name: "flask", alt: "flask-icon", type: "framework"
+    },
+    go: {
+        icon: goIcon, name: "go", alt: "go-icon", type: "language"
+    },
+    rails: {
+        icon: railsIcon, name: "rails", alt: "rails-icon", type: "framework"
+    },
+    react: {
+        icon: reactIcon, name: "react", alt: "react-icon", type: "framework"
+    },
+    ruby: {
+        icon: rubyIcon, name: "ruby", alt: "ruby-icon", type: "language"
+    },
+    sql: {
+        icon: sqlIcon, name: "sql", alt: "sql-icon", type: "language"
+    }
+}
